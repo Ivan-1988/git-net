63394ff352620ce7fb53754e1a7c2de7f74bf2d9
Ivan-1988 <eskimosik-vap@mail.ru>
2021-04-14 20:47:51 +0300

    step 25

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -25,7 +25,7 @@ function Message(props: MessagePropsType) {
 
 function Dialogs() {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Ivan'},
         {id: 2, name: 'Art'},
         {id: 3, name: 'Sveta'},
@@ -34,7 +34,7 @@ function Dialogs() {
         {id: 6, name: 'Valera'}
     ]
 
-    let messagesData = [
+    let messages = [
         {id: 1, message: 'Hi'},
         {id: 2, message: 'How is your it-kamasutra?'},
         {id: 3, message: 'Yo'},
@@ -42,16 +42,19 @@ function Dialogs() {
         {id: 5, message: 'Yo'}
     ]
 
+    let dialogsElements = dialogs
+        .map( d => <DialogItem name={d.name} id={d.id}/>);
+
+    let messagesElements = messages
+        .map(m => <Message message={m.message}/>)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
-                <Message message={messagesData[2].message}/>
+                {messagesElements}
             </div>
         </div>
     )
